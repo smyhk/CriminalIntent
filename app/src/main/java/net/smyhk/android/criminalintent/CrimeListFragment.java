4641d3b51b7fefddc089ac20dcf958fff7b72139
Steve Kedzie <seal.team.smyhk@gmail.com>
2017-03-08 16:49:01 -0800

    include extra for CriminalActivity intent

--- app/src/main/java/net/smyhk/android/criminalintent/CrimeListFragment.java
+++ app/src/main/java/net/smyhk/android/criminalintent/CrimeListFragment.java
@@ -70,7 +70,7 @@ public class CrimeListFragment extends Fragment {
             //Toast.makeText(getActivity(),
             //        mCrime.getTitle() + " clicked!", Toast.LENGTH_SHORT)
             //        .show();
-            Intent intent = new Intent(getActivity(), CrimeActivity.class);
+            Intent intent = CrimeActivity.newIntent(getActivity(), mCrime.getId());
             startActivity(intent);
         }
     }
