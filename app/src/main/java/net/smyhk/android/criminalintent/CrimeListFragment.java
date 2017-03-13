d36345074bc4892bfb4cc90f46242dfb68361e14
Steve Kedzie <seal.team.smyhk@gmail.com>
2017-03-13 16:11:18 -0700

    retain count visibility in toolbar when back pressed

--- app/src/main/java/net/smyhk/android/criminalintent/CrimeListFragment.java
+++ app/src/main/java/net/smyhk/android/criminalintent/CrimeListFragment.java
@@ -23,6 +23,8 @@ public class CrimeListFragment extends Fragment {
     private boolean mSubtitleVisible;
     private final int REQUEST_CRIME = 1;
 
+    private static final String SAVED_SUBTITLE_VISIBLE = "subtitle";
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,6 +40,10 @@ public class CrimeListFragment extends Fragment {
                 .findViewById(R.id.crime_recycler_view);
         mCrimeRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
 
+        if (savedInstanceState != null) {
+            mSubtitleVisible = savedInstanceState.getBoolean(SAVED_SUBTITLE_VISIBLE);
+        }
+
         updateUI();
 
         return view;
@@ -47,6 +53,12 @@ public class CrimeListFragment extends Fragment {
     public void onResume() {
         super.onResume();
         updateUI();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putBoolean(SAVED_SUBTITLE_VISIBLE, mSubtitleVisible);
     }
 
     @Override
@@ -105,6 +117,7 @@ public class CrimeListFragment extends Fragment {
             mAdapter.notifyDataSetChanged();
         }
 
+        updateSubtitle();
     }
 
     private class CrimeHolder extends RecyclerView.ViewHolder
