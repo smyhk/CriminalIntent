8b4642cc87c998f971e017a48af990cff7831e5b
Steve Kedzie <seal.team.smyhk@gmail.com>
2017-03-19 13:16:49 -0700

    add database object

--- app/src/main/java/net/smyhk/android/criminalintent/CrimeLab.java
+++ app/src/main/java/net/smyhk/android/criminalintent/CrimeLab.java
@@ -1,6 +1,9 @@
 package net.smyhk.android.criminalintent;
 
 import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
+import net.smyhk.android.criminalintent.database.CrimeBaseHelper;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,6 +17,8 @@ public class CrimeLab {
     private static CrimeLab sCrimeLab;
 
     private List<Crime> mCrimes;
+    private Context mContext;
+    private SQLiteDatabase mDatabase;
 
     public static CrimeLab get(Context context) {
         if (sCrimeLab == null) {
@@ -24,6 +29,8 @@ public class CrimeLab {
 
     // note private constructor; denotes singleton
     private CrimeLab(Context context) {
+        mContext = context.getApplicationContext();
+        mDatabase = new CrimeBaseHelper(mContext).getWritableDatabase();
         mCrimes = new ArrayList<>();
     }
 
